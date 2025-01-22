9b8d37f30aea4c9fde5860dc655a2634bd1dc369
João Victor <jvdematos004@gmail.com>
2025-01-22 08:34:38 -0300

    ContaPagar | ContaReceber

---
+++ src/main/java/com/mentoria/lojavirtual/model/ContaPagar.java
@@ -0,0 +1,149 @@
+package com.mentoria.lojavirtual.model;
+
+import com.mentoria.lojavirtual.enums.StatusContaPagar;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+
+@jakarta.persistence.Entity
+@Table(name = "conta_pagar")
+@SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
+public class ContaPagar implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
+    private Long id;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal valorTotal;
+
+    private BigDecimal valorDesconto;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusContaPagar status;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dtVencimento;
+
+    @Temporal(TemporalType.DATE)
+    private Date dtPagamento;
+
+    @ManyToOne(targetEntity = PessoaFisica.class)
+    @JoinColumn(name = "pessoa_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private PessoaFisica pessoa;
+
+    @ManyToOne(targetEntity = PessoaJuridica.class)
+    @JoinColumn(name = "pessoa_forn_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_forn_fk"))
+    private PessoaJuridica pessoa_fornecedor;
+
+
+    @ManyToOne(targetEntity = PessoaJuridica.class)
+    @JoinColumn(name = "empresa_id", nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_id_fk"))
+    private PessoaJuridica empresa;
+
+
+    public PessoaJuridica getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(PessoaJuridica empresa) {
+        this.empresa = empresa;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public StatusContaPagar getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusContaPagar status) {
+        this.status = status;
+    }
+
+    public PessoaJuridica getPessoa_fornecedor() {
+        return pessoa_fornecedor;
+    }
+
+    public void setPessoa_fornecedor(PessoaJuridica pessoa_fornecedor) {
+        this.pessoa_fornecedor = pessoa_fornecedor;
+    }
+
+    public Date getDtVencimento() {
+        return dtVencimento;
+    }
+
+    public void setDtVencimento(Date dtVencimento) {
+        this.dtVencimento = dtVencimento;
+    }
+
+    public Date getDtPagamento() {
+        return dtPagamento;
+    }
+
+    public void setDtPagamento(Date dtPagamento) {
+        this.dtPagamento = dtPagamento;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public BigDecimal getValorDesconto() {
+        return valorDesconto;
+    }
+
+    public void setValorDesconto(BigDecimal valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
+
+    public PessoaFisica getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(PessoaFisica pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ContaPagar that = (ContaPagar) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
+
